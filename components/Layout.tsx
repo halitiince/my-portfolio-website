eb856710bfa2b9468adbbaa0b6dfffb2c79c1376
Halit Ince <hltnc.zxc@gmail.com>
2021-11-22 16:44:54 +0300

    fix: fix main tag styling

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import {NextPage} from "next";
 import Header from "./Header";
 
-const Layout: NextPage = () => {
+const Layout: NextPage = ({children}) => {
     return (
-        <div className="min-h-screen bg-gray-900">
+        <div className="min-h-screen bg-gray-900 flex flex-col">
             <Header/>
+            <main className="flex flex-col justify-center w-full max-w-2xl px-4 mx-auto mt-8">{children}</main>
         </div>
     );
 };
