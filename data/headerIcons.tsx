c7f24991ca9a1c66e1441298cbbab5e740bef840
Halit Ince <hltnc.zxc@gmail.com>
2021-11-22 16:44:37 +0300

    feat: add ui data

--- data/headerIcons.tsx
+++ data/headerIcons.tsx
@@ -15,7 +15,7 @@ export const SunIcon: React.FC = () => {
 export const MoonIcon: React.FC = () => {
     return (
         <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 20 20"
-             className="text-gray-600 text-2xl" height="1em" width="1em"
+             className="text-gray-200 text-2xl" height="1em" width="1em"
              xmlns="http://www.w3.org/2000/svg">
             <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"/>
         </svg>
---
+++ data/skillElements.tsx
@@ -0,0 +1,55 @@
+import JavaIcon from "../public/icons/java.svg"
+import JavaScriptIcon from "../public/icons/javascript.svg"
+import TypeScriptIcon from "../public/icons/typescript.svg"
+import CssIcon from "../public/icons/css3.svg"
+import DockerIcon from "../public/icons/docker.svg"
+import GitIcon from "../public/icons/git.svg"
+import HtmlIcon from "../public/icons/html5.svg"
+import PostgreIcon from "../public/icons/postgresql.svg"
+import PythonIcon from "../public/icons/python.svg"
+import ReactIcon from "../public/icons/react.svg"
+
+const itemList = [
+    {
+        name: "Java",
+        icon: <img src={JavaIcon} alt="icon" />
+    },
+    {
+        name: "JavaScript",
+        icon: <img src={JavaScriptIcon} alt="icon" />
+    },
+    {
+        name: "TypeScript",
+        icon: <img src={TypeScriptIcon} alt="icon" />
+    },
+    {
+        name: "CSS3",
+        icon: <img src={CssIcon} alt="icon" />
+    },
+    {
+        name: "Docker",
+        icon: <img src={DockerIcon} alt="icon" />
+    },
+    {
+        name: "Git",
+        icon: <img src={GitIcon} alt="icon" />
+    },
+    {
+        name: "HTML5",
+        icon: <img src={HtmlIcon} alt="icon" />
+    },
+    {
+        name: "PostgreSQL",
+        icon: <img src={PostgreIcon} alt="icon" />
+    },
+    {
+        name: "Python",
+        icon: <img src={PythonIcon} alt="icon" />
+    },
+    {
+        name: "ReactJS",
+        icon: <img src={ReactIcon} alt="icon" />
+    }
+]
+
+export default itemList
