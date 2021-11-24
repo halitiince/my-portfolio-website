4ee8c418a83d77ccfe3fe37ab54d08c7ac437591
Halit Ince <hltnc.zxc@gmail.com>
2021-11-24 16:51:02 +0300

    style: fix footer to bottom

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {NextPage} from "next";
 import Header from "./Header";
+import Footer from "./Footer";
 
 const Layout: NextPage = ({children}) => {
     return (
-        <div className="min-h-screen bg-gray-900 flex flex-col">
-            <Header/>
-            <main className="flex flex-col justify-center w-full max-w-2xl px-4 mx-auto mt-8">{children}</main>
+        <div className="min-h-screen bg-gray-900 flex flex-col justify-between">
+            <div>
+                <Header/>
+                <main className="flex flex-col justify-center w-full max-w-2xl px-4 mx-auto mt-8">{children}</main>
+            </div>
+            <Footer />
         </div>
     );
 };
