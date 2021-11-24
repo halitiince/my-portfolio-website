0fb51513a04727d8cd42cc9364e41458221d6a2e
Halit Ince <hltnc.zxc@gmail.com>
2021-11-24 16:49:29 +0300

    feat: create Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <div className="mt-2 py-8 px-4 flex flex items-center justify-start text-gray-500 w-full max-w-2xl mx-auto">
+            <p>© 2021 Halit Ince. All rights reserved.</p>
+        </div>
+    );
+};
+
+export default Footer;
