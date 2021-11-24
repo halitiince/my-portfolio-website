eb9ad0d943f14d2c669c5b36b2a3e6c5705b84e7
Halit İnce <halit.ince00@gmail.com>
2021-11-24 22:43:17 +0300

    fix: icon coloring in prod

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,11 +12,12 @@ const Home: NextPage = () => {
                 <Image className="rounded-full" src={avatar} width="144" height="144" alt="home page avatar"/>
             </div>
             <div className="mt-4">
-                <h2 className="text-primary text-lg uppercase font-semibold">Hey!
+                <div className="flex flex-row items-end">
+                    <h2 className="text-primary text-lg uppercase font-semibold">Hey!</h2>
                     <span className="text-3xl ml-2">
                         👋
                     </span>
-                </h2>
+                </div>
                 <h1 className="text-white text-5xl font-extrabold pt-2 hover:text-primary">It's Halit.</h1>
                 <ul className="ml-2 pt-2 list-disc list-inside text-gray-400 text-lg">
                     <li className="pt-1">
@@ -37,13 +38,16 @@ const Home: NextPage = () => {
                 </ul>
             </div>
             <div className="mt-8">
-                <h2 className="text-white font-extrabold text-2xl">
-                    Tech Stack 🎒
-                </h2>
+                <div className="flex flex-row items-end">
+                    <h2 className="text-white font-extrabold text-2xl">
+                        Tech Stack
+                    </h2>
+                    <span className="text-2xl ml-2"> 🎒 </span>
+                </div>
                 <div className="grid grid-cols-3 gap-x-3 gap-y-2 mt-4 ">
                     {
                         skillElements.map((element) => {
-                            return <SkillItem name={element.name} icon={element.icon} />
+                            return <SkillItem key={element.name} name={element.name} icon={element.icon} />
                         })
                     }
                 </div>
