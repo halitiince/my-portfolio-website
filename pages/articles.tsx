bca6bf818a83eacbf7e64c8956fa098257be82e5
Halit Ince <hltnc.zxc@gmail.com>
2021-11-26 15:20:03 +0300

    feat: create articles page for articles with dummy data

---
+++ pages/articles.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ArticleElement from "../components/ArticleElement";
+
+const articles = [{
+        style: "primary",
+        data: {
+            title: "Blogpost Title 1",
+            desc: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
+            link: "#"
+        }
+    },
+    {
+        style: "secondary",
+        data: {
+            title: "Blogpost Title 2",
+            desc: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
+            link: "#"
+        }
+    },
+    {
+        style: "primary",
+        data: {
+            title: "Blogpost Title 3",
+            desc: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
+            link: "#"
+        }
+    }]
+
+const Articles: React.FC = () => {
+    return (
+        <div className="flex flex-col justify-center w-full max-w-2xl px-4 mx-auto">
+            <h1 className="text-primary font-bold text-2xl uppercase mb-4">Articles
+                <span className="text-2xl ml-2">📖</span></h1>
+            {
+                articles.map((article) => {
+                    return <ArticleElement key={article.data.title} style={article.style} data={article.data} />
+                })
+            }
+        </div>
+    );
+}
+
+export default Articles;
