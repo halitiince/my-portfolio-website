19a47f7fb9ca232eabcdb5556cece6d8743a69a4
Halit Ince <hltnc.zxc@gmail.com>
2021-11-30 15:44:47 +0300

    feat: create custom 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import Image from 'next/image';
+import errorCat from '../public/img/error_cat.png';
 
 const ErrorPage: React.FC = () => {
     return (
-        <div className="bg-gray-900 text-white flex flex-col justify-center w-full max-w-2xl px-4 mx-auto mt-8">
-            <h1 className="text-secondary font-extrabold text-5xl mb-6">Ooopps!</h1>
+        <div className="flex flex-col justify-center w-full max-w-2xl px-4 mx-auto mt-8">
+            <h1 className="text-cpink font-extrabold text-5xl mb-6">Ooopps!</h1>
+            <Image src={errorCat} />
             <p className="text-gray-400 text-2xl mb-6 font-semibold">Something went wrong... The page you were trying to reach doesn't exist.</p>
             <a className="text-primary font-semibold text-3xl cursor-pointer" href="/">Go back Home -{'>'}</a>
         </div>
     );
-};
+}
 
 export default ErrorPage;
