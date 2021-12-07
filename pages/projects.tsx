2464635139d2de9c546f17ac66b59996ae9dfe40
Halit Ince <hltnc.zxc@gmail.com>
2021-12-07 16:47:47 +0300

    style: trying some new theme and stylings

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -12,14 +12,14 @@ const Projects: React.FC = () => {
             {
                 projectsData.map(project => {
                     return (
-                        <div key={project.name} className="flex bg-white rounded-xl max-h-30 mt-4  cursor-pointer transform transition duration-500 hover:scale-105">
-                            <Image className="max-h-xl rounded-l-xl" src={spotifyImage} alt="project image ss"/>
-                            <div className="py-2 px-2 flex-grow flex flex-col justify-between">
+                        <div key={project.name} className="grid grid-cols-3 bg-white rounded-xl max-h-30 mt-4  cursor-pointer transform transition duration-500 hover:scale-105">
+                            <Image className="col-span-1 rounded-l-xl" src={spotifyImage} alt="project image ss"/>
+                            <div className="py-2 px-2 col-span-2 flex flex-col justify-between">
                                 <div>
                                     <h1 className="text-3xl text-darkBg font-extrabold">{project.name}</h1>
                                     <p className="text-sm text-gray-900 font-bold pt-1">{project.description}</p>
                                 </div>
-                                <div className="flex flex-wrap  items-center">
+                                <div className="flex flex-wrap items-center">
                                     {
                                         project.techStack.map((tech) => {
                                             return <p className="flex-grow text-center bg-gray-400 rounded-full py-2 px-3 mr-1.5 mt-2 text-darkBg font-semibold text-sm">{tech}</p>
